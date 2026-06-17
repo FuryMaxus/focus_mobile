2a1454fd1840df00c8fc1e5ec672ac3847556d9a
Jose Valenzuela <jivalenzuela2010@hotmail.com>
2026-06-17 17:09:02 -0400

    re added toalExpGained to SyncResponse

--- app/src/main/java/com/example/focus/data/remote/SyncDto.kt
+++ app/src/main/java/com/example/focus/data/remote/SyncDto.kt
@@ -24,6 +24,7 @@ data class SyncResponse(
     val status: String,
     val processedSessionsCount: Int,
     val totalExp: Int,
+    val totalExpGained: Int,
     val currentLevel: Int,
     val leveledUp: Boolean,
     val levelsGained: Int,
