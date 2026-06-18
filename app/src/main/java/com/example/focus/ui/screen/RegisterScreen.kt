f97c8464bf1cdca88f564220ce6954cabb8702f6
Jose Valenzuela <jivalenzuela2010@hotmail.com>
2026-06-18 18:36:04 -0400

    moved OrnamentalDivider.kt to components package

--- app/src/main/java/com/example/focus/ui/screen/RegisterScreen.kt
+++ app/src/main/java/com/example/focus/ui/screen/RegisterScreen.kt
@@ -20,7 +20,7 @@ import androidx.compose.ui.unit.sp
 
 import com.example.focus.viewmodel.AuthViewModel
 import com.example.focus.ui.theme.*
-import com.example.focus.ui.OrnamentalDivider
+import com.example.focus.ui.component.OrnamentalDivider
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 
 
