f8da85c81dd688de95958dd051a08a111c272007
Jose Valenzuela <jivalenzuela2010@hotmail.com>
2026-06-18 20:47:26 -0400

    forgot to extend the database with the class of room, now id fix. Added the module to inyect the db with hilt

--- app/src/main/java/com/example/focus/FocusApp.kt
+++ app/src/main/java/com/example/focus/FocusApp.kt
@@ -1,7 +1,20 @@
 package com.example.focus
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class FocusApp : Application()
+class FocusApp : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+
+}
--- app/src/main/java/com/example/focus/data/local/FocusDatabase.kt
+++ app/src/main/java/com/example/focus/data/local/FocusDatabase.kt
@@ -1,11 +1,12 @@
 package com.example.focus.data.local
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.example.focus.data.local.dao.FocusSessionDao
 import com.example.focus.data.local.entity.FocusSessionEntity
 
 @Database(entities = [FocusSessionEntity::class], version = 1, exportSchema = false)
-abstract class FocusDatabase {
+abstract class FocusDatabase : RoomDatabase() {
 
     abstract fun focusSessionDao(): FocusSessionDao
 
